dcb1fcd0c95956f29c5a1ae533303d8bf1dc6491
kimskogsmo <76793289+kimskogsmo@users.noreply.github.com>
2021-03-21 18:04:33 +0100

    Create add-context-menu-items.bat

---
+++ add-context-menu-items.bat
@@ -0,0 +1,16 @@
+Windows Registry Editor Version 5.00
+[HKEY_CLASSES_ROOT\*\shell\Open with VS Code]
+@="Edit file in VS Code"
+"Icon"="C:\\Program Files\\Microsoft VS Code\\Code.exe,0"
+[HKEY_CLASSES_ROOT\*\shell\Open with VS Code\command]
+@="\"C:\\Program Files\\Microsoft VS Code\\Code.exe\" \"%1\""
+[HKEY_CLASSES_ROOT\Directory\shell\vscode]
+@="Open folder as VS Code Project"
+"Icon"="\"C:\\Program Files\\Microsoft VS Code\\Code.exe\",0"
+[HKEY_CLASSES_ROOT\Directory\shell\vscode\command]
+@="\"C:\\Program Files\\Microsoft VS Code\\Code.exe\" \"%1\""
+[HKEY_CLASSES_ROOT\Directory\Background\shell\vscode]
+@="Open this folder as a project in VS Code"
+"Icon"="\"C:\\Program Files\\Microsoft VS Code\\Code.exe\",0"
+[HKEY_CLASSES_ROOT\Directory\Background\shell\vscode\command]
+@="\"C:\\Program Files\\Microsoft VS Code\\Code.exe\" \"%V\""
